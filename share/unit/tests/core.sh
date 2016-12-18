54b0f2b925b02478be2dd549cebaa1e009f132dd
Nima Talebi <github@nima.id.au>
2016-12-17 16:05:00 -0800

    Added a search test for bad associative arrays usage

--- share/unit/tests/core.sh
+++ share/unit/tests/core.sh
@@ -57,6 +57,12 @@ function testCoreGlobal() {
     #assertEquals "1.4" 2600 $v
 }
 
+function testCoreUnsupportedAssociativeArrayAssignments() {
+    local vetted
+    vetted="$(git grep -E '[a-zA-Z0-9]+\+=\( *\[' | md5sum | cut -d' ' -f1)"
+    assertEquals '0.0.1' '09a2684a0023bdd670ad455efbd74d8e' "${vetted}"
+}
+
 function exitWith() {
   g_CACHE_OUT || {
     md5sum <<< "$(date +%N)" | cut -b 1-32
