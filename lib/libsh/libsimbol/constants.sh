1e643d4bc01e336052dd2dd62caaad8c0d080198
Nima Talebi <ntd@amazon.com>
2017-06-12 21:52:05 +0000

    lib/libsh/libsimbol/constants.sh

--- lib/libsh/libsimbol/constants.sh
+++ lib/libsh/libsimbol/constants.sh
@@ -1,5 +1,5 @@
 # vim: tw=0:ts=4:sw=4:et:ft=bash
-#shellcheck disable=SC2034
+#shellcheck disable=SC2034,SC2155
 
 set -f
 set -u
@@ -8,57 +8,73 @@ set -u
 if [ ${SOURCED_CONSTANTS:-0} -eq 0 ]; then
 
 #. Constants -={
-export SIMBOL_VERSION="1.0-rc2"
-export SIMBOL_DATE_FORMAT="%x-%X"
+declare -r SIMBOL_VERSION="1.0-rc3"; export SIMBOL_VERSION
+declare -r SIMBOL_DATE_FORMAT="%x-%X"; export SIMBOL_DATE_FORMAT
+declare BASH_DECLARE_GLOBAL_OPTS=g
 
 #. Magic Numbers -={
-declare -rig FD_STDIN=0; export FD_STDIN
-declare -rig FD_STDOUT=1; export FD_STDOUT
-declare -rig FD_STDERR=2; export FD_STDERR
 
-true; declare -rig TRUE=$?; export TRUE
-false; declare -rig FALSE=$?; export FALSE
+case ${BASH_VERSINFO[0]}:${BASH_VERSINFO[1]}:${BASH_VERSINFO[2]} in
+    4:[01]:*) BASH_DECLARE_GLOBAL_OPTS=;;
+esac
+export BASH_DECLARE_GLOBAL_OPTS
+
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} FD_STDIN=0; export FD_STDIN
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} FD_STDOUT=1; export FD_STDOUT
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} FD_STDERR=2; export FD_STDERR
+
+true; declare -ri${BASH_DECLARE_GLOBAL_OPTS} TRUE=$?; export TRUE
+false; declare -ri${BASH_DECLARE_GLOBAL_OPTS} FALSE=$?; export FALSE
 declare -A SIMBOL_BOOL=( [false]=${FALSE?} [true]=${TRUE?} ); export SIMBOL_BOOL
 
-declare -rig CODE_SUCCESS=0x00; export CODE_SUCCESS
-declare -rig CODE_FAILURE=0x01; export CODE_FAULIRE
-declare -rig CODE_CANCELS=0x82; export CODE_CANCELS
-declare -rig CODE_WARNING=0xf0; export CODE_WARNING
 
-#. 64..127 Internal
-export CODE_NOTIMPL=64
-export CODE_DISABLED=66
-export CODE_USAGE_SHORT=90
-export CODE_USAGE_MODS=91
-export CODE_USAGE_MOD=92
-export CODE_USAGE_FN_GUESS=93
-#CODE_USAGE_FN_SHORT=94
-export CODE_USAGE_FN_LONG=95
+#. 0x00..0x1f General Error Codes -={
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_SUCCESS=0x00; export CODE_SUCCESS
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_FAILURE=0x01; export CODE_FAILURE
 
-export CODE_IMPORT_GOOOD=${CODE_SUCCESS} #. good module
-export CODE_IMPORT_ERROR=96              #. invalid/bad module (can't source/parse)
-export CODE_IMPORT_ADMIN=97              #. administratively disabled
-export CODE_IMPORT_UNDEF=98              #. no such module
-export CODE_IMPORT_UNSET=99              #. no module set
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E01=0x01; export CODE_E01
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E02=0x02; export CODE_E02
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E03=0x03; export CODE_E03
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E04=0x04; export CODE_E04
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E05=0x05; export CODE_E05
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E06=0x06; export CODE_E06
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E07=0x07; export CODE_E07
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E08=0x08; export CODE_E08
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E09=0x09; export CODE_E09
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E10=0x0a; export CODE_E10
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E11=0x0b; export CODE_E11
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E12=0x0c; export CODE_E12
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E13=0x0d; export CODE_E13
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E14=0x0e; export CODE_E14
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_E15=0x0f; export CODE_E15
+#. }=-
+#. 0x20..0x2f Module Import Codes -={
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_IMPORT_GOOOD=0x00; export CODE_IMPORT_GOOOD      #. good module; export CODE_IMPORT_GOOOD
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_IMPORT_ERROR=0x20; export CODE_IMPORT_ERROR      #. invalid/bad module (can't source/parse); export CODE_IMPORT_ERROR
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_IMPORT_ADMIN=0x21; export CODE_IMPORT_ADMIN      #. administratively disabled; export CODE_IMPORT_ADMIN
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_IMPORT_UNDEF=0x22; export CODE_IMPORT_UNDEF      #. no such module; export CODE_IMPORT_UNDEF
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_IMPORT_UNSET=0x23; export CODE_IMPORT_UNSET      #. no module set; export CODE_IMPORT_UNSET
+#. }=-
+#. 0x30..0x3f Usage Codes -={
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_NOTIMPL=0x30; export CODE_NOTIMPL
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_DISABLED=0x31; export CODE_DISABLED
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_USAGE_SHORT=0x32; export CODE_USAGE_SHORT
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_USAGE_MODS=0x33; export CODE_USAGE_MODS
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_USAGE_MOD=0x34; export CODE_USAGE_MOD
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_USAGE_FN_GUESS=0x34; export CODE_USAGE_FN_GUESS
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_USAGE_FN_LONG=0x35; export CODE_USAGE_FN_LONG
+#. }=-
+#. 0x40..0x4f Special Non-Failure Codes -={
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_WARNING=0x40; export CODE_WARNING
+#. }=-
+#. 0x80..0x8f OS Reserved Codes -={
+declare -ri${BASH_DECLARE_GLOBAL_OPTS} CODE_CANCELS=0x82; export CODE_CANCELS
+#. }=-
 
-#. 128..255 General Error Codes
-export CODE_E01=128
-export CODE_E02=129
-export CODE_E03=130
-export CODE_E04=131
-export CODE_E05=132
-export CODE_E06=133
-export CODE_E07=134
-export CODE_E08=135
-export CODE_E09=136
+declare -ri CODE_DEFAULT=CODE_USAGE_FN_LONG; export CODE_DEFAULT
 
-SIMBOL_DELIM="$(printf "\x07")"
-export SIMBOL_DELIM
-SIMBOL_DELOM="$(printf "\x08")"
-export SIMBOL_DELOM
-
-# shellcheck disable=SC2034
-export CODE_DEFAULT=${CODE_USAGE_FN_LONG?}
+declare -r SIMBOL_DELIM="$(printf "\x07")"; export SIMBOL_DELIM
+declare -r SIMBOL_DELOM="$(printf "\x08")"; export SIMBOL_DELOM
 
 #. }=-
 
