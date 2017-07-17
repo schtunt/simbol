9d3e7a5bd9e22c1f953a99fa89843031130b4c24
Martin Lehmann <martin@ml-solutions.org.au>
2017-07-17 15:19:11 +1000

    Added more ldap static unit tests

--- share/unit/tests/ldap-static.sh
+++ share/unit/tests/ldap-static.sh
@@ -4,6 +4,16 @@ core:import ldap
 #. ldap -={
 function ldapOneTimeSetUp() {
     export SIMBOL_PROFILE=UNITTEST
+    mock:clear
+    mock:write <<-!MOCK
+        declare -a USER_LDAPHOSTS=( ldap1.mockery.net ldap2.mockery.net ldap3.mockery.net )
+        declare -a USER_LDAPHOSTS_RW=( ldap1.rw.mockery.net ldap2.rw.mochery.net )
+        declare USER_GDN="ou=groups,dc=mockery,dc=net"
+        export USER_LDAPHOSTS USER_LDAPHOSTS_RW
+	function :vault:read() { return "pa55w0rd; }
+	function ldapsearch() { return true; }
+!MOCK
+
 }
 
 function ldapSetUp() {
@@ -20,19 +30,46 @@ function ldapOneTimeTearDown() {
 
 #. testCoreLdapHostInternal -={
 function testCoreLdapHostInternal() {
-    : pass
+    local rv
+    # random one
+    mock:wrapper ldap :host >"${stdoutF?}" 2>"${stderrF?}"
+    assertTrue "${FUNCNAME[0]}/1.1" $?
+    rv="$(cat "${stdoutF}")"
+    assertEquals "${FUNCNAME[0]}/1.2" mockery.net "${rv#*.}"
+
+    # 2nd one
+    mock:wrapper ldap :host 1 >"${stdoutF?}" 2>"${stderrF?}"
+    assertTrue "${FUNCNAME[0]}/1.3" $?
+    rv="$(cat "${stdoutF}")"
+    assertEquals "${FUNCNAME[0]}/1.4" ldap2.mockery.net "${rv}"
+
+    # bad index one
+    mock:wrapper ldap :host 4 >"${stdoutF?}" 2>"${stderrF?}"
+    assertFalse "${FUNCNAME[0]}/1.5" $?
+
+    # global one
+    #mock:wrapper ldap :host >"${stdoutF?}" 2>"${stderrF?}"
+    #assertTrue "${FUNCNAME[0]}/1.6" $?
+    #rv="$(cat "${stdoutF}")"
+    #assertEquals "${FUNCNAME[0]}/1.7" globalldap.mockery.net "${rv}"
 }
 #. }=-
 
 #. testCoreLdapHost_rwInternal -={
 function testCoreLdapHost_rwInternal() {
+    #local rv
+    #mock:wrapper ldap :host_rw >"${stdoutF?}" 2>"${stderrF?}"
+    #assertTrue "${FUNCNAME[0]}/1.1" $?
+    #rv="$(cat "${stdoutF}")"
+    #assertEquals "${FUNCNAME[0]}/1.2" rw.mockery.net "${rv#*.}"
     : pass
 }
 #. }=-
 
 #. testCoreLdapAuthenticateInternal -={
 function testCoreLdapAuthenticateInternal() {
-    : pass
+    mock:wrapper ldap :authenticate >"${stdoutF?}" 2>"${stderrF?}"
+    assertTrue "${FUNCNAME[0]}/1.1" $?
 }
 #. }=-
 
